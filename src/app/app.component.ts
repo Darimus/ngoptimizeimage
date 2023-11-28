6fdae2c4778eb13958bc5b505a96bfb7e13dc501
Darimus <bruno.passos@outlook.com.br>
2023-11-28 10:28:30 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'newapp';
+
+  constructor() {}
+  @Input() ngSrcset!: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  IMAGE_LOADER,
+  ImageLoaderConfig,
+  NgOptimizedImage,
+  provideCloudflareLoader,
+  provideImgixLoader,
+} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
+  providers: [
+    provideImgixLoader('http://localhost:4200/assets/'),
+    /* {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        const img = config.src.split('.');
+        const name = img.shift();
+        const type = img.pop();
+        const width = config.width;
+        return `http://localhost:4200/assets/${name}-${width}w.${type}`;
+      },
+    } */
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
